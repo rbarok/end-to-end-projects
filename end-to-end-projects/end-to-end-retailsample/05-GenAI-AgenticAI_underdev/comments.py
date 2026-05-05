cec5604cbe8955e61a98eac1253172d5e20e0371
rbarok <barroco.dev@gmail.com>
2026-05-05 06:43:25 +0000

    Cleaning

---
+++ end-to-end-projects/end-to-end-retailsample/05-GenAI-AgenticAI_underdev/comments.py
@@ -0,0 +1,5 @@
+To do:
+    
+1) Review dataset, add metadata, semantic relationships, simple RAG using Vector Search DB;
+2) Genie Space;
+3) 
--- end-to-end-projects/end-to-end-structure/05-GenAI-AgenticAI/Finishing-end-to-end.py
+++
@@ -1,9 +0,0 @@
-To be continued ::
-
-Simple RAG; different datasets; text, imagine, possible videos all inside Vector Database;
-
-Add Vectorless; Hierarchically organizing a group of documents; 
-
-Add KG-RAG; finding unkwown relationship between tables; through semantically similar entities; tagging, description, metadadata in general;
-
-Challenge: Can I build an index of graphs? PageIndex for KG-RAG?
